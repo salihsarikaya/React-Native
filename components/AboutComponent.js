2105c38ffbb0d300bf04bf254b89a7050785f776
Salih Sarikaya <salihsarikaya34@gmail.com>
2020-10-24 10:42:33 -0700

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
-class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    static navigationOptions = {
-        title: "About Us",
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
     };
+};
+
+function Mission() {
+    return(
+        <Card title='Our Mission'>
+            <text style={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </text>
+        </Card>
+    );
+}
+class About extends Component {
+    
+    static navigationOptions = {
+        title: 'About Us'
+    };
+
     render() {
-        return <ScrollView></ScrollView>;
+        const renderPartner = ({item}) => {
+            return (
+                <ListItem
+                title={item.name}
+                subtitle={item.description}
+                leftAvatar={{source: {uri: baseUrl + item.image}}}
+                />
+            );
+        };
+
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                title="Community Partners">
+                    <FlatList 
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </Card>
+            </ScrollView>
+        );
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+
     static navigationOptions = {
-        title: "Contact Us",
-    };
-    render() {
-        return <ScrollView></ScrollView>;
+        title: 'Contact Us'
     }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card
+                title="Contact Information"
+                wrapperStyle={{margin: 20}}>
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text>U.S.A.</Text>
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+                </Card>
+            </ScrollView>
+        );
+    };
 }
 
 export default Contact;
