f99298a7a47656dc3770e8f30c45f4103a0d4b83
Salih Sarikaya <salihsarikaya34@gmail.com>
2020-11-07 08:03:09 -0800

    Secure Store Working

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -7,23 +7,6 @@ import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite, postComment } from '../redux/ActionCreators';
 import * as Animatable from 'react-native-animatable';
-import * as ActionTypes from "./ActionTypes";
-
-export const comments = (state = { errMess: null, comments: [] }, action) => {
-    switch (action.type) {
-    case ActionTypes.ADD_COMMENTS:
-        return { ...state, errMess: null, comments: action.payload };
-    case ActionTypes.COMMENTS_FAILED:
-        return { ...state, errMess: action.payload };
-    case ActionTypes.ADD_COMMENT:
-        const comment = action.payload
-        comment.id=state.comments.length
-        const comments=[...state.comments, comment]
-            return {...state, errMess: null,comments }
-    default:
-        return state;
-    }
-};
 
 const mapStateToProps = state => {
     return {
